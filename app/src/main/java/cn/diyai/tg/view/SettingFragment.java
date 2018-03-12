05f835b981b12653c5535e7ecf0120a6e759d747
Patrick <wxmgcs@sina.cn>
2018-03-12 23:19:40 +0800

    1.设置就寝时间 2.设置时间颗粒度

--- app/src/main/java/cn/diyai/tg/view/SettingFragment.java
+++ app/src/main/java/cn/diyai/tg/view/SettingFragment.java
@@ -12,6 +12,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import cn.diyai.tg.R;
 import cn.diyai.tg.model.Constants;
@@ -54,6 +56,7 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.setting_sleeptime:
+                setSleepTime();
                 break;
             case R.id.setting_time_particle:
                 setTimeParticle();
@@ -62,7 +65,55 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
 
     }
 
+    /**
+     * 设置就寝时间
+     */
+    public void setSleepTime(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View view = inflater.inflate(R.layout.setting_alertdialog_sleeptime, null);
+        TimePicker beginTime = (TimePicker) view.findViewById(R.id.setting_sleeptime_begintime);
+        TimePicker endTime = (TimePicker) view.findViewById(R.id.setting_sleeptime_endtime);
+        String begin = null;
+        String end = null;
+        beginTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener(){
+            @Override
+            public void onTimeChanged(TimePicker view
+                    , int hourOfDay, int minute){
+                Log.i(Constants.TAG,"begin:"+hourOfDay+":"+minute);
+                setting.setSleepTime(hourOfDay+":"+minute);
+            }
+        });
+
+        endTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener(){
+            @Override
+            public void onTimeChanged(TimePicker view
+                    , int hourOfDay, int minute){
+                Log.i(Constants.TAG,"end:"+hourOfDay+":"+minute);
+                setting.setSleepTime(setting.getSleepTime()+"~"+hourOfDay+":"+minute);
+            }
+        });
+
+        builder.setView(view);
+        builder.setMessage(R.string.setting_sleeptime)
+                .setPositiveButton(R.string.btn_sure, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    settingPresenter.updateSetting(setting);
+                    }
+                })
+                .setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+    /**
+     * 设置时间颗粒度
+     */
     public void setTimeParticle() {
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View view = inflater.inflate(R.layout.setting_alertdialog_timeparticle, null);
@@ -71,6 +122,7 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, timeParticles);
         spinner.setAdapter(adapter);
+        spinner.setSelection(getTimeParticleIndex(timeParticles),true);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -95,5 +147,14 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
                 });
         builder.create();
         builder.show();
+    }
+
+    private int getTimeParticleIndex(String[] timeParticles){
+        for(int i =0;i < timeParticles.length;i++){
+            if(setting.getTimeParticle() == Integer.parseInt(timeParticles[i])){
+                return i;
+            }
+        }
+        return 0;
     }
 }
